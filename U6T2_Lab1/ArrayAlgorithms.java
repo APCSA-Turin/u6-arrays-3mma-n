d5f1400d08471b5c9ff0863e5e8b12dce1494b37
3mma-n <148897839+3mma-n@users.noreply.github.com>
2024-12-12 16:57:10 +0000

    12/12

--- U6T2_Lab1/ArrayAlgorithms.java
+++ U6T2_Lab1/ArrayAlgorithms.java
@@ -103,5 +103,129 @@ public class ArrayAlgorithms {
         return newArr;
     }
 
+    /** Changes the first and last element of nums to be num
+     *
+     *  THIS METHOD MUTATES (MODIFIES) ORIGINAL ARRAY
+     *  PRECONDITION: nums.length > 0
+     */
+    public static void bookEnd(int[] nums, int num) {
+        nums[0] = num;
+        nums[nums.length - 1] = num;
+    }
 
+    /** Multiplies each number in numList by multiplier.
+     *
+     *  THIS METHOD MUTATES (MODIFIES) ORIGINAL ARRAY
+     *  PRECONDITION: numList.length > 0
+     */
+    public static void multiplyBy(int[] numList, int multiplier) {
+        for (int i = 0; i < numList.length; i++) {
+            numList[i] *= multiplier;
+        }
+    }
+
+    /** Returns an array of ints where each element is the product
+     *  of the corresponding element in numList and multiplier.
+     *
+     *  THIS METHOD DOES NOT MUTATE (MODIFY) ORIGINAL ARRAY
+     *  PRECONDITION: numList.length > 0
+     */
+    public static int[] multiplyByNoModify(int[] numList, int multiplier) {
+        int[] newList = new int[numList.length];
+        for (int i = 0; i < numList.length; i++) {
+            newList[i] = numList[i] * multiplier;
+        }
+        return newList;
+    }
+
+    /** Adds a "!" to the end of each String in wordList.  However, if a
+     *  a string already ends in an exclamation point, do NOT add another.
+     *
+     *  THIS METHOD MUTATES (MODIFIES) ORIGINAL ARRAY
+     *  PRECONDITION: wordList.length > 0
+     */
+    public static void addExclamation(String[] wordList) {
+
+        for (int i = 0; i < wordList.length; i++) {
+            if (!wordList[i].substring(wordList[i].length() - 1).equals("!")) {
+                wordList[i] += "!";
+            }
+        }
+    }
+
+    /** Returns an array of booleans where each element reflects whether
+     *  the corresponding element in tempList is at or below the freezing
+     *  temperature of 32 (true) or not (false).
+     *
+     *  THIS METHOD DOES NOT MUTATE (MODIFY) ORIGINAL ARRAY
+     *  PRECONDITION: tempList.length > 0
+     */
+    public static boolean[] isFreezing(int[] tempList) {
+        boolean[] output = new boolean[tempList.length];
+        for (int i = 0; i < tempList.length; i++) {
+            if (tempList[i] <= 32) {
+                output[i] = true;
+            } else {
+                output[i] = false;
+            }
+        }
+        return output;
+    }
+
+    /** Shifts all elements in numList to the LEFT one index
+     *
+     *  The first element in the array should wrap around
+     *  to the back of the array.  For example, given the array
+     *  {5, 1, 3, 4, 7, 6}, this method modifies the array
+     *  to {1, 3, 4, 7, 6, 5} (all elements shifted left one
+     *  with the last element wrapping around to the back)
+     *
+     *  THIS METHOD MUTATES (MODIFIES) ORIGINAL ARRAY
+     *  PRECONDITION: numList.length > 0
+     */
+    public static void shiftLeft(int[] numList) {
+        int temp = numList[0];
+        for (int i = 0; i < numList.length - 1; i++) {
+            numList[i] = numList[i + 1];
+        }
+        numList[numList.length - 1] = temp;
+    }
+
+    /** Shifts all elements in numList to the RIGHT one index.
+     *
+     *  The last element in the array should wrap around
+     *  to the front of the array.  For example, given the array
+     *  {5, 1, 3, 4, 7, 6}, this method modifies the array
+     *  to {6, 5, 1, 3, 4, 7} (all elements shifted right one
+     *  with the last element wrapping around to the front)
+     *
+     *  THIS METHOD MUTATES (MODIFIES) ORIGINAL ARRAY
+     *  PRECONDITION: numList.length > 0
+     */
+    public static void shiftRight(int[] numList) {
+        int temp = numList[numList.length - 1];
+        for (int i = numList.length - 1; i > 0; i--) {
+            numList[i] = numList[i - 1];
+        }
+        numList[0] = temp;
+    }
+
+    /** Reverses all elements in an array.
+     *
+     *  For example, given the array {5, 1, 3, 4, 7, 6}, this method
+     *  modifies the array to {6, 7, 4, 3, 1, 5}.
+     *  Should work with all array lengths (both odd and even).
+     *
+     *  THIS METHOD MUTATES (MODIFIES) ORIGINAL ARRAY
+     *  PRECONDITION: numList.length > 0
+     */
+    public static void reverse(int[] numList) {
+        int[] revList = new int[numList.length];
+        for (int i = 0; i < numList.length; i++) {
+            revList[i] = numList[numList.length - 1 - i];
+        }
+        for (int j = 0; j < revList.length; j++) {
+            numList[j] = revList[j];
+        }
+    }
 }
